84b5df7b51f5226e43822a4c0a85a4f734dab2bd
Erickspresso <131963609+Erickspresso@users.noreply.github.com>
2023-06-21 11:02:46 +0700

    Added new files

---
+++ kontak.php
@@ -0,0 +1,33 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail ->isSMTP();
+$mail ->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "lahmanasayatahu25@gmail.com";
+$mail->Password = "cpldbcydtuzljqbw";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("erickpaskahbawoleh@gmail.com");
+
+$mail->Subject = $subject;
+$mail->Body = $message. "\nfrom,\n". $email;
+
+$mail->send();
+
+echo "Email sent";
